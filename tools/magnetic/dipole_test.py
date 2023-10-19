27970b8b61bf60c4ae5fcc09dc4d70e147969b69
Revixa1 <txguimont@hotmail.com>
2023-10-19 17:59:38 -0400

    added B's vector (live)graphs

--- tools/magnetic/dipole_test.py
+++ tools/magnetic/dipole_test.py
@@ -8,22 +8,32 @@ from matplotlib import cm
 rx,ry,rz= 0,0,0.3
 mu0=10e-4
 
-lowdist,highdist,ddist=-6,6,0.5
+lowdist,highdist,ddist=-3,3,0.1
 datapoints=int((highdist-lowdist)/ddist)
 
-pausetime=0.01
+pausetime=0.001
 
 
 fig=plt.figure()
 ax = fig.add_subplot(111,projection='3d')
+
+fig2=plt.figure()
+bx= fig2.add_subplot(3,1,1)
+by=fig2.add_subplot(3,1,2)
+bz=fig2.add_subplot(3,1,3)
+
 plt.ion()
 
 for rz in np.arange(lowdist,highdist,ddist):
 
-    DX=np.empty((1,0),dtype=float)
-    DY=np.empty((1,0),dtype=float)
+    DX=np.empty((0,0),dtype=float)
+    DY=np.empty((0,0),dtype=float)
+    BX=np.empty((0,0),dtype=float)
+    BY=np.empty((0,0),dtype=float)
+    BZ=np.empty((0,0),dtype=float)
     DZ=np.empty((0,0),dtype=float)
-    ic(DZ.shape)
+    
+    #ic(DZ.shape)
     
     
     
@@ -37,33 +47,70 @@ for rz in np.arange(lowdist,highdist,ddist):
     for rx in np.arange(lowdist,highdist,ddist):
         loop_DZ=np.empty((0),dtype=float)
         
+        
         for ry in np.arange(lowdist,highdist,ddist):
+
+               
+                
+                
     
                 r_vector=np.array([rx,ry,rz]).reshape(3,1)
                 r_scalar=np.linalg.norm(r_vector)
                 r_unit=r_vector/ np.linalg.norm(r_vector)
                 
-                m=np.array([0,0.5,-0.5]).reshape(1,3)
+                m=np.array([0,0,1]).reshape(1,3)
                 
-                B=  (mu0/(4*np.pi) * (3 *((m.dot(r_vector))-m)/r_scalar**3)).reshape(3,1)
+                B=  (mu0/(4*np.pi) * (3 *((m.dot(r_vector)).dot(r_unit.reshape(1,3))-m)/r_scalar**3)).reshape(3,1)
                 #ic(B)
                 
                 DX=np.append(DX,rx)
                 DY=np.append(DY,ry)
+                BX=np.append(BX,B[0])
+                BY=np.append(BY,B[1])
+                BZ=np.append(BZ,B[2])
+
+                #bx.plot(DY,BX)
+                #by.plot(DY,BY)
+                #bz.plot(DY,BZ)
+                #plt.pause(pausetime)
                 
                 loop_DZ=np.append(loop_DZ,np.linalg.norm(B))
-                ic(loop_DZ.shape)
+
+
+               
+                #ic(loop_DZ.shape)
                 #ax.plot(rx,ry,zs=B[2,:],marker='*',color='b')
                 #ax.plot(rx,ry,np.linalg.norm(B),marker='o',color='b')
+
+
+        bx.set(ylabel="Bx(T)")
+        by.set(ylabel="By(T)")
+        bz.set(ylabel="Bz(T)",xlabel="posY(m)")
+        
+        
+        bx.plot(DY,BX)
+        by.plot(DY,BY)
+        bz.plot(DY,BZ)
+        plt.pause(pausetime)
+        bx.clear()
+        by.clear()
+        bz.clear()
+        BX=np.empty((0,0),dtype=float)
+        BY=np.empty((0,0),dtype=float)
+        BZ=np.empty((0,0),dtype=float)
+        DY=np.empty((0,0),dtype=float)
+
+
         DZ=np.append(DZ,loop_DZ)
-        ic(DZ)
+        #ic(DZ)
         i=i+1
 
-    ic(datapoints)    
+    #ic(datapoints)    
     DZ=DZ.reshape(i,datapoints) 
     
-    ic(DZ.shape)  
-    ax.plot_surface(X,Y,DZ,cmap=cm.coolwarm,linewidth=0, antialiased=False)
+    #ic(DZ.shape)
+    ax.clear()
+    ax.plot_surface(X,Y,DZ,cmap=cm.viridis,linewidth=0, antialiased=False)
     plt.show()
     plt.pause(pausetime)
-    plt.cla()
+    
